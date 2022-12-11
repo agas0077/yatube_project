7a0f5efb24acef7f7bfa112da50a4db4946a1ecd
Андрей Агасьянц <at-am@yandex.ru>
2022-12-11 17:32:32 +0300

    Fix author name in templates

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -20,8 +20,8 @@ class Post(models.Model):
 
 
 class Group(models.Model):
-    title = models.CharField(max_length=50)
-    slug = models.SlugField()
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
     description = models.TextField()
 
     def __str__(self):
